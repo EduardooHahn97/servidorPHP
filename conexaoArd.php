2dae8395c874c594e6d9256e240ff24364327b31
Eduardo <eduardo.hahn14@hotmail.com>
2022-04-07 21:33:13 -0300

    banco heroku

--- conexaoArd.php
+++ conexaoArd.php
@@ -6,9 +6,9 @@
 	header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 	header('Content-Type: application/json; charset=utf-8');*/
 	
-	$username = "root";
-	$password = null;
-	$conn = new PDO('mysql:host=localhost;dbname=aplicativo', $username, $password);
+	$username = "b63fbaa8783c7a";
+	$password = "dd8c51c3";
+	$conn = new PDO('mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_e38dc7531122647', $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
 ?>
